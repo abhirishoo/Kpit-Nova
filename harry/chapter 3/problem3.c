4d7b17451dbac9fa739544ab5661f3b7cfb7e941
Abhijeet Rishoo <abhi.rishoo2003@gmail.com>
2025-01-25 04:17:54 +0530

    Guessing Game Project 1

--- harry/chapter 3/problem3.c
+++ harry/chapter 3/problem3.c
@@ -6,29 +6,28 @@
         Above 10.0L      30% 
     Note that there is no tax below 2.5L. Take income amount as an input from the user.
 */
-
 #include<stdio.h>
 int main(){
     float income;
-    float tax;
-    printf("Enter income in lacs : ");
+    float tax = 0;
+    printf("Enter income in lacs: ");
     scanf("%f", &income);
-    if(income>2.5 && income<5.0){
-        tax =  ((income-2.5)*0.05)*100000;
-         printf("Tax given to govt. will be : %f", tax);
+
+    if (income <= 2.5) {
+        printf("Tax given to govt. will be: 0\n");
     }
-    if(income>5.0 && income<10.0){
-       tax =  ((income-5)*0.2 + 2.5*0.05)*100000;
-        printf("Tax given to govt. will be : %f", tax);
+    else if (income > 2.5 && income <= 5.0) {
+        tax = (income - 2.5) * 0.05 * 100000;
+        printf("Tax given to govt. will be: %.2f\n", tax);
     }
-    if(income>10.0){
-       tax =  ((income-10)*0.3 + 2.5*0.05 + 5*0.2)*100000;
-        printf("Tax given to govt. will be : %f", tax);
-    }else{
-        printf("0 tax");
+    else if (income > 5.0 && income <= 10.0) {
+        tax = ((income - 5.0) * 0.20 + 2.5 * 0.05) * 100000;
+        printf("Tax given to govt. will be: %.2f\n", tax);
     }
-    
-    
+    else if (income > 10.0) {
+        tax = ((income - 10.0) * 0.30 + 2.5 * 0.05 + 5.0 * 0.20) * 100000;
+        printf("Tax given to govt. will be: %.2f\n", tax);
+    }
 
     return 0;
 }
