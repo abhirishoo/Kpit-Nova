2a4c2ead328b8f894317a71bad79a3e4f4db7cda
Abhijeet Rishoo <101319292+abhirishoo@users.noreply.github.com>
2025-01-28 03:42:10 +0530

    Delete harry directory

--- harry/chapter 3/problem1.c
+++
@@ -1,6 +0,0 @@
-// 1. What will be the output of this program
-// int a = 10;
-// if (a = 11)
-// printf("I am 11");  ✅Answerr
-// else
-// printf("I am not 11");
--- harry/chapter 4/problem3.c
+++
@@ -1,5 +0,0 @@
-// 3. A do while loop is executed:
-// a. At least once. ✅
-// b. At least twice.
-// c. At most once.
-
