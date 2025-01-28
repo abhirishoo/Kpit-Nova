f7640c51b6e81cd26a33b84cecba68261840dfa0
Abhijeet Rishoo <abhi.rishoo2003@gmail.com>
2025-01-28 11:36:08 +0530

    final commit

--- harry/chapter 2/problem5.c
+++
@@ -1,4 +0,0 @@
-// 5. 3.0 + 1 will be:
-// a. Integer.
-// b. Floating point number. correct
-// c. Character.
--- harry/chapter 4/problem4.c
+++
@@ -1,3 +0,0 @@
-// 4. What can be done using one type of loop can also be done using the other two
-// types of loops – true or false?
-// -true
